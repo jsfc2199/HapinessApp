ae4ad4a94edb8076d076395b4110a6012b58d921
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-21 20:18:04 -0500

    nav bar
    acercamiento al navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ToolbarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
