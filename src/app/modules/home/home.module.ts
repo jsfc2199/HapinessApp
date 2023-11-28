72eb204488def4fa5b8690e27d0e110bc2762703
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-28 16:26:56 -0500

    Tabla con Material
    Se corrigen imports y se añaden nuevos para poder tener la información preliminar bien mostrada en la app

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { PeopleTableComponent } from 'src/app/components';
 
 
 
@@ -11,7 +12,8 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    PeopleTableComponent
   ]
 })
 export class HomeModule { }
